f19425cb8c33bd6f3e062c9ae19fb0d92fe9c368
Rubén Pérez Mercado <ruben.perezmercado@alum.uca.es>
2025-08-02 14:49:00 +0200

    fix: ignore order when comparing the files in a directory in tests

--- reana_jupyterlab/tests/test_files.py
+++ reana_jupyterlab/tests/test_files.py
@@ -41,19 +41,30 @@ def filespace():
     shutil.rmtree(base_path)
 
 @pytest.mark.parametrize('path, expected', [
-    ('pytest_workspace', {'entries': [{ 'name': 'file6.yaml', 'type': 'file', 'path': 'pytest_workspace/file6.yaml'}, {'name': 'folder1', 'type': 'directory', 'path': 'pytest_workspace/folder1'}]}),
-    ('pytest_workspace/folder1', {'entries': [{'name': 'folder2', 'type': 'directory', 'path': 'pytest_workspace/folder1/folder2'}]}),
-    ('pytest_workspace/folder1/folder2', {'entries': [{'name': 'file3.yaml', 'type': 'file', 'path': 'pytest_workspace/folder1/folder2/file3.yaml'}, {'name': 'folder3', 'type': 'directory', 'path': 'pytest_workspace/folder1/folder2/folder3'}]}),
-    ('pytest_workspace/folder1/folder2/folder3', {'entries': []}),
-    ('pytest_workspace/folder4', ''),
-    ('pytest_workspace/file4.java', ''),
-    ('pytest_workspace/folder1/../file6.yaml', '')
+    ('pytest_workspace', set([('folder1', 'directory', 'pytest_workspace/folder1'), ('file6.yaml', 'file', 'pytest_workspace/file6.yaml')])),
+    ('pytest_workspace/folder1', set([('folder2', 'directory', 'pytest_workspace/folder1/folder2')])),
+    ('pytest_workspace/folder1/folder2', set([('file3.yaml', 'file', 'pytest_workspace/folder1/folder2/file3.yaml'), ('folder3', 'directory', 'pytest_workspace/folder1/folder2/folder3')])),
+    ('pytest_workspace/folder1/folder2/folder3', set()),
+    ('pytest_workspace/folder4', set()),
+    ('pytest_workspace/file4.java', set()),
+    ('pytest_workspace/folder1/../file6.yaml', set())
 ])
 async def test_get_files(jp_fetch, path, expected, filespace):
     try:
         response = await jp_fetch(ENDPOINT, params={'path': path})
         assert response.code == 200
+
         data = json.loads(response.body)
-        assert data == expected
+
+        assert 'entries' in data
+        entries = data['entries']
+
+        assert isinstance(entries, list)
+
+        received = set((entry['name'], entry['type'], entry['path']) for entry in entries)
+        assert received == expected
+
+        
     except Exception as e:
+        print(e)
         assert e.code == 404
